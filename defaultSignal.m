8dcd4a9f3d9f13ea4d1e7163bb5ec85f22e3a91b
Pramesh Sherla <pramesherla121@gmail.com>
2020-12-05 19:19:35 +0530

    Simulation run. Simulation run with default values and a sub-system to emulate noise is made.

---
+++ defaultSignal.m
@@ -0,0 +1,13 @@
+%% defining the Parameters for signal.
+h2r = 2 * pi;
+Fs = 48000;
+tStop = 1;
+dt = 1/Fs;
+
+%% Default values for signals.
+
+signalFreq = 1;
+noiseFreq = 50;
+
+signalAmp = 10;
+noiseAmp = 1;
