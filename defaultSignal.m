82dc179e421104399bca5cd34dbc3cb335190088
Pramesh Sherla <pramesherla121@gmail.com>
2020-12-05 20:00:50 +0530

    Updated sript to reliase frequence response and step response.

--- defaultSignal.m
+++ defaultSignal.m
@@ -11,3 +11,12 @@ noiseFreq = 50;
 
 signalAmp = 10;
 noiseAmp = 1;
+
+%% making the discreate transfer function 
+
+sys = tf([1 0.4],[1 -0.5 0.25],dt,'Variable','z^-1');
+
+figure(1); bode(sys);
+figure(2); step(sys);
+
+
